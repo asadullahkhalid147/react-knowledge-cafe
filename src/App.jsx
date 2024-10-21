d5ee1641634cf7bd09ac243235bd9b6b9db3cb02
asadullahkhalid147 <asadullahkhalid147@gmail.com>
2024-10-22 01:57:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './component/Header/Header'
+import Blogs from './component/Blogs/Blogs'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header></Header>
+      <Blogs></Blogs>
+     
+    </>
+  )
+}
+
+export default App
